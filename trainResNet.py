6c3a1750911f09a301447a06fbad8fb0a47420be
ham952 <ham952@hotmail.com>
2019-11-16 16:42:42 +0500

    xbd Resnet101 for Thesis

---
+++ trainResNet.py
@@ -0,0 +1,419 @@
+# USAGE
+# python train.py --checkpoints output/checkpoints
+# python train.py --checkpoints output/checkpoints \
+# 	--model output/checkpoints/epoch_40.hdf5 --start-epoch 40
+# python train.py --checkpoints output/checkpoints \
+#       --model output/checkpoints/epoch_50.hdf5 --start-epoch 50
+
+# set the matplotlib backend so figures can be saved in the background
+from __future__ import print_function
+import matplotlib
+matplotlib.use("Agg")
+import keras
+import argparse
+from pyimagesearch.callbacks.epochcheckpoint import EpochCheckpoint
+from pyimagesearch.callbacks.trainingmonitor import TrainingMonitor
+from keras.layers import Dense, Conv2D, BatchNormalization, Activation
+from keras.layers import AveragePooling2D, Input, Flatten
+from keras.optimizers import Adam, SGD
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers import l2
+from keras import backend as K
+from keras.models import Model, load_model
+from keras.datasets import cifar10,fashion_mnist
+import cv2
+import sys
+import os
+import numpy as np
+import keras.backend as K
+from sklearn.preprocessing import LabelBinarizer
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--checkpoints", required=True,
+	help="path to output checkpoint directory")
+ap.add_argument("-m", "--model", type=str,
+	help="path to *specific* model checkpoint to load")
+ap.add_argument("-s", "--start-epoch", type=int, default=0,
+	help="epoch to restart training at")
+args = vars(ap.parse_args())
+
+# Training parameters
+batch_size = 128  
+epochs = 200
+data_augmentation = True
+num_classes = 10
+
+# Subtracting pixel mean improves accuracy
+subtract_pixel_mean = True
+
+# Model parameter
+# ----------------------------------------------------------------------------
+#           |      | 200-epoch | Orig Paper| 200-epoch | Orig Paper| sec/epoch
+# Model     |  n   | ResNet v1 | ResNet v1 | ResNet v2 | ResNet v2 | GTX1080Ti
+#           |v1(v2)| %Accuracy | %Accuracy | %Accuracy | %Accuracy | v1 (v2)
+# ----------------------------------------------------------------------------
+# ResNet20  | 3 (2)| 92.16     | 91.25     | -----     | -----     | 35 (---)
+# ResNet32  | 5(NA)| 92.46     | 92.49     | NA        | NA        | 50 ( NA)
+# ResNet44  | 7(NA)| 92.50     | 92.83     | NA        | NA        | 70 ( NA)
+# ResNet56  | 9 (6)| 92.71     | 93.03     | 93.01     | NA        | 90 (100)
+# ResNet110 |18(12)| 92.65     | 93.39+-.16| 93.15     | 93.63     | 165(180)
+# ResNet164 |27(18)| -----     | 94.07     | -----     | 94.54     | ---(---)
+# ResNet1001| (111)| -----     | 92.39     | -----     | 95.08+-.14| ---(---)
+# ---------------------------------------------------------------------------
+
+n = 12
+
+# Model version
+# Orig paper: version = 1 (ResNet v1), Improved ResNet: version = 2 (ResNet v2)
+version = 2
+
+# Computed depth from supplied model parameter n
+if version == 1:
+    depth = n * 6 + 2
+elif version == 2:
+    depth = n * 9 + 2
+
+# Model name, depth and version
+model_type = 'ResNet%dv%d' % (depth, version)
+
+# grab the Fashion MNIST dataset (if this is your first time running
+# this the dataset will be automatically downloaded)
+print("[INFO] loading Fashion MNIST...")
+((trainX, trainY), (testX, testY)) = fashion_mnist.load_data()
+
+
+# Fashion MNIST images are 28x28 but the network we will be training
+# is expecting 32x32 images
+trainX = np.array([cv2.resize(x, (32, 32)) for x in trainX])
+testX = np.array([cv2.resize(x, (32, 32)) for x in testX])
+
+# scale data to the range of [0, 1]
+trainX = trainX.astype("float32") / 255.0
+testX = testX.astype("float32") / 255.0
+
+# reshape the data matrices to include a channel dimension (required
+# for training)
+trainX = trainX.reshape((trainX.shape[0], 32, 32, 1))
+testX = testX.reshape((testX.shape[0], 32, 32, 1))
+
+# If subtract pixel mean is enabled
+if subtract_pixel_mean:
+    trainX_mean = np.mean(trainX, axis=0)
+    trainX -= trainX_mean
+    testX -= trainX_mean
+
+# Input image dimensions.
+input_shape = trainX.shape[1:]
+
+''' 
+# convert the labels from integers to vectors
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+'''
+print('trainX shape:', trainX.shape)
+print(trainX.shape[0], 'train samples')
+print(testX.shape[0], 'test samples')
+print('trainY shape:', trainY.shape)
+
+# Convert class vectors to binary class matrices.
+trainY = keras.utils.to_categorical(trainY, num_classes)
+testY = keras.utils.to_categorical(testY, num_classes)
+
+# construct the image generator for data augmentation
+aug = ImageDataGenerator(width_shift_range=0.1,
+	height_shift_range=0.1, horizontal_flip=True,
+	fill_mode="nearest")
+
+# build the path to the training plot and training history
+plotPath = os.path.sep.join(["output", "resnet_fashion_mnist.png"])
+jsonPath = os.path.sep.join(["output", "resnet_fashion_mnist.json"])
+
+# construct the set of callbacks
+callbacks = [
+	EpochCheckpoint(args["checkpoints"], every=5,
+		startAt=args["start_epoch"]),
+	TrainingMonitor(plotPath,
+		jsonPath=jsonPath,
+		startAt=args["start_epoch"]),
+	ReduceLROnPlateau(monitor = "val_loss",
+                     factor=0.1,
+                     cooldown=0,
+                     patience=2,
+                     epsilon = 1e-04,
+                     min_lr=0.5e-6,
+                     verbose = 1) ]
+
+###########################################################################		]
+# Construct Resnet Models
+###########################################################################
+        
+def resnet_layer(inputs,
+                 num_filters=16,
+                 kernel_size=3,
+                 strides=1,
+                 activation='relu',
+                 batch_normalization=True,
+                 conv_first=True):
+    """2D Convolution-Batch Normalization-Activation stack builder
+
+    # Arguments
+        inputs (tensor): input tensor from input image or previous layer
+        num_filters (int): Conv2D number of filters
+        kernel_size (int): Conv2D square kernel dimensions
+        strides (int): Conv2D square stride dimensions
+        activation (string): activation name
+        batch_normalization (bool): whether to include batch normalization
+        conv_first (bool): conv-bn-activation (True) or
+            bn-activation-conv (False)
+
+    # Returns
+        x (tensor): tensor as input to the next layer
+    """
+    conv = Conv2D(num_filters,
+                  kernel_size=kernel_size,
+                  strides=strides,
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=l2(1e-4))
+
+    x = inputs
+    if conv_first:
+        x = conv(x)
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+    else:
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+        x = conv(x)
+    return x
+
+
+def resnet_v1(input_shape, depth, num_classes=10):
+    """ResNet Version 1 Model builder [a]
+
+    Stacks of 2 x (3 x 3) Conv2D-BN-ReLU
+    Last ReLU is after the shortcut connection.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filters is
+    doubled. Within each stage, the layers have the same number filters and the
+    same number of filters.
+    Features maps sizes:
+    stage 0: 32x32, 16
+    stage 1: 16x16, 32
+    stage 2:  8x8,  64
+    The Number of parameters is approx the same as Table 6 of [a]:
+    ResNet20 0.27M
+    ResNet32 0.46M
+    ResNet44 0.66M
+    ResNet56 0.85M
+    ResNet110 1.7M
+
+    # Arguments
+        input_shape (tensor): shape of input image tensor
+        depth (int): number of core convolutional layers
+        num_classes (int): number of classes (CIFAR10 has 10)
+
+    # Returns
+        model (Model): Keras model instance
+    """
+    if (depth - 2) % 6 != 0:
+        raise ValueError('depth should be 6n+2 (eg 20, 32, 44 in [a])')
+    # Start model definition.
+    num_filters = 16
+    num_res_blocks = int((depth - 2) / 6)
+
+    inputs = Input(shape=input_shape)
+    x = resnet_layer(inputs=inputs)
+    # Instantiate the stack of residual units
+    for stack in range(3):
+        for res_block in range(num_res_blocks):
+            strides = 1
+            if stack > 0 and res_block == 0:  # first layer but not first stack
+                strides = 2  # downsample
+            y = resnet_layer(inputs=x,
+                             num_filters=num_filters,
+                             strides=strides)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters,
+                             activation=None)
+            if stack > 0 and res_block == 0:  # first layer but not first stack
+                # linear projection residual shortcut connection to match
+                # changed dims
+                x = resnet_layer(inputs=x,
+                                 num_filters=num_filters,
+                                 kernel_size=1,
+                                 strides=strides,
+                                 activation=None,
+                                 batch_normalization=False)
+            x = keras.layers.add([x, y])
+            x = Activation('relu')(x)
+        num_filters *= 2
+
+    # Add classifier on top.
+    # v1 does not use BN after last shortcut connection-ReLU
+    x = AveragePooling2D(pool_size=8)(x)
+    y = Flatten()(x)
+    outputs = Dense(num_classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal')(y)
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def resnet_v2(input_shape, depth, num_classes=10):
+    """ResNet Version 2 Model builder [b]
+
+    Stacks of (1 x 1)-(3 x 3)-(1 x 1) BN-ReLU-Conv2D or also known as
+    bottleneck layer
+    First shortcut connection per layer is 1 x 1 Conv2D.
+    Second and onwards shortcut connection is identity.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filter maps is
+    doubled. Within each stage, the layers have the same number filters and the
+    same filter map sizes.
+    Features maps sizes:
+    conv1  : 32x32,  16
+    stage 0: 32x32,  64
+    stage 1: 16x16, 128
+    stage 2:  8x8,  256
+
+    # Arguments
+        input_shape (tensor): shape of input image tensor
+        depth (int): number of core convolutional layers
+        num_classes (int): number of classes (CIFAR10 has 10)
+
+    # Returns
+        model (Model): Keras model instance
+    """
+    if (depth - 2) % 9 != 0:
+        raise ValueError('depth should be 9n+2 (eg 56 or 110 in [b])')
+    # Start model definition.
+    num_filters_in = 16
+    num_res_blocks = int((depth - 2) / 9)
+
+    inputs = Input(shape=input_shape)
+    # v2 performs Conv2D with BN-ReLU on input before splitting into 2 paths
+    x = resnet_layer(inputs=inputs,
+                     num_filters=num_filters_in,
+                     conv_first=True)
+
+    # Instantiate the stack of residual units
+    for stage in range(3):
+        for res_block in range(num_res_blocks):
+            activation = 'relu'
+            batch_normalization = True
+            strides = 1
+            if stage == 0:
+                num_filters_out = num_filters_in * 4
+                if res_block == 0:  # first layer and first stage
+                    activation = None
+                    batch_normalization = False
+            else:
+                num_filters_out = num_filters_in * 2
+                if res_block == 0:  # first layer but not first stage
+                    strides = 2    # downsample
+
+            # bottleneck residual unit
+            y = resnet_layer(inputs=x,
+                             num_filters=num_filters_in,
+                             kernel_size=1,
+                             strides=strides,
+                             activation=activation,
+                             batch_normalization=batch_normalization,
+                             conv_first=False)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters_in,
+                             conv_first=False)
+            y = resnet_layer(inputs=y,
+                             num_filters=num_filters_out,
+                             kernel_size=1,
+                             conv_first=False)
+            if res_block == 0:
+                # linear projection residual shortcut connection to match
+                # changed dims
+                x = resnet_layer(inputs=x,
+                                 num_filters=num_filters_out,
+                                 kernel_size=1,
+                                 strides=strides,
+                                 activation=None,
+                                 batch_normalization=False)
+            x = keras.layers.add([x, y])
+
+        num_filters_in = num_filters_out
+
+    # Add classifier on top.
+    # v2 has BN-ReLU before Pooling
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = AveragePooling2D(pool_size=8)(x)
+    y = Flatten()(x)
+    outputs = Dense(num_classes,
+                    activation='softmax',
+                    kernel_initializer='he_normal')(y)
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+        
+###########################################################################		
+###########################################################################
+
+
+# if there is no specific model checkpoint supplied, then initialize
+# the network (ResNet) and compile the model
+if args["model"] is None:
+    print("[INFO] compiling model...")
+
+    if version == 2:
+        model = resnet_v2(input_shape=input_shape, depth=depth)
+    else:
+        model = resnet_v1(input_shape=input_shape, depth=depth)
+        
+    opt = SGD(lr=1e-1)
+    model.compile(loss="categorical_crossentropy", optimizer=opt,
+            metrics=["accuracy"])
+    model.summary()
+    print(model_type)
+
+# otherwise, we're using a checkpoint model
+else:
+    # load the checkpoint from disk
+    print("[INFO] loading {}...".format(args["model"]))
+    model = load_model(args["model"])
+
+    # update the learning rate
+    print("[INFO] old learning rate: {}".format(
+            K.get_value(model.optimizer.lr)))
+    K.set_value(model.optimizer.lr, 1e-2)
+    print("[INFO] new learning rate: {}".format(
+            K.get_value(model.optimizer.lr)))
+
+
+
+
+# train the network
+print("[INFO] training network...")
+model.fit_generator(
+	aug.flow(trainX, trainY, batch_size=128),
+	validation_data=(testX, testY),
+        shuffle=True,
+	steps_per_epoch=len(trainX) // batch_size,
+	epochs=epochs,
+	callbacks=callbacks,
+	verbose=1)
+
+# Score trained model.
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
